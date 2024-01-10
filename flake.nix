fd5bb0bdd6eafc811c9ddb8e13e81937d3d6d824
basilgood <elsile69@yahoo.com>
2024-01-10 13:59:05 +0200

    feat(web): add jsonlint

--- flake.nix
+++ flake.nix
@@ -34,6 +34,7 @@
         packages = with pkgs; [
           nodejs_20
           nodePackages_latest.typescript-language-server
+          nodePackages_latest.jsonlint
           vscode-langservers-extracted
           jq
           yamlfix
