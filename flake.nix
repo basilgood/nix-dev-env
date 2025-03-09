4c89a66c9e27b90b4c137849318a4ca1c3c80bbb
basilgood <elsile69@yahoo.com>
2025-03-10 00:46:41 +0200

    chore: config with flake-utils

--- flake.nix
+++ flake.nix
@@ -4,96 +4,88 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     devshell.url = "github:numtide/devshell";
+    flake-utils.url = "github:numtide/flake-utils";
   };
   outputs =
     {
-      self,
-      nixpkgs,
       devshell,
+      flake-utils,
+      nixpkgs,
+      ...
     }:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (
-        system:
-        import nixpkgs {
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
           inherit system;
           overlays = [ devshell.overlays.default ];
-        }
-      );
-    in
-    {
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          default = pkgs.devshell.mkShell {
-            packages = with pkgs; [
-              nixfmt-rfc-style
-              nixd
-            ];
-            env = [
-              {
-                name = "DEVSHELL_NO_MOTD";
-                value = 1;
-              }
-            ];
-          };
-          web = pkgs.devshell.mkShell {
-            packages = with pkgs; [
-              nodejs_22
-              nodePackages.typescript-language-server
-              vscode-langservers-extracted
-              vtsls
-              jq
-              yamllint
-              shfmt
-              azure-storage-azcopy
-            ];
-            env = [
-              {
-                name = "DEVSHELL_NO_MOTD";
-                value = 1;
-              }
-              {
-                name = "PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS";
-                value = 1;
-              }
-              {
-                name = "PATH";
-                prefix = "node_modules/.bin";
-              }
-            ];
-          };
-          nvim = pkgs.devshell.mkShell {
-            packages = with pkgs; [
-              lua-language-server
-              lua54Packages.luacheck
-              stylua
-              gcc
-              gnumake
-            ];
-            env = [
-              {
-                name = "DEVSHELL_NO_MOTD";
-                value = 1;
-              }
-            ];
-          };
-          rust = pkgs.devshell.mkShell {
-            name = "rust";
-            imports = [ "${devshell}/extra/language/rust.nix" ];
-            packages = with pkgs; [ rust-analyzer ];
-            env = [
-              {
-                name = "RUST_BACKTRACE";
-                value = "1";
-              }
-            ];
-          };
-        }
-      );
-    };
+        };
+      in
+      {
+        devShells.default = pkgs.devshell.mkShell {
+          packages = with pkgs; [
+            nixfmt-rfc-style
+            nixd
+          ];
+          env = [
+            {
+              name = "DEVSHELL_NO_MOTD";
+              value = 1;
+            }
+          ];
+        };
+        devShells.web = pkgs.devshell.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+            nodePackages.typescript-language-server
+            vscode-langservers-extracted
+            vtsls
+            jq
+            yamllint
+            shfmt
+            azure-storage-azcopy
+          ];
+          env = [
+            {
+              name = "DEVSHELL_NO_MOTD";
+              value = 1;
+            }
+            {
+              name = "PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS";
+              value = 1;
+            }
+            {
+              name = "PATH";
+              prefix = "node_modules/.bin";
+            }
+          ];
+        };
+        devShells.nvim = pkgs.devshell.mkShell {
+          packages = with pkgs; [
+            lua-language-server
+            lua54Packages.luacheck
+            stylua
+            gcc
+            gnumake
+          ];
+          env = [
+            {
+              name = "DEVSHELL_NO_MOTD";
+              value = 1;
+            }
+          ];
+        };
+        devShells.rust = pkgs.devshell.mkShell {
+          name = "rust";
+          imports = [ "${devshell}/extra/language/rust.nix" ];
+          packages = with pkgs; [ rust-analyzer ];
+          env = [
+            {
+              name = "RUST_BACKTRACE";
+              value = "1";
+            }
+          ];
+        };
+      }
+    );
 }
