7b55fdeeb649fcae8fe87a169f427c13669cb5ae
basilgood <elsile69@yahoo.com>
2026-01-29 14:47:39 +0200

    feat: replace devshell with mkShell and add linear-term support

--- flake.nix
+++ flake.nix
@@ -1,42 +1,36 @@
 {
   description = "development environments";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    devshell = {
-      url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
+    linear-term = {
+      url = "github:tjburch/linear-term";
+      flake = false;
     };
   };
   outputs =
     {
-      devshell,
       nixpkgs,
+      linear-term,
       ...
     }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [ devshell.overlays.default ];
       };
+      pythonPackages = pkgs.python3Packages;
     in
     {
       devShells.${system} = {
-        default = pkgs.devshell.mkShell {
+        default = pkgs.mkShell {
           packages = with pkgs; [
             nixfmt
             nixd
             nil
           ];
-          env = [
-            {
-              name = "DEVSHELL_NO_MOTD";
-              value = 1;
-            }
-          ];
         };
-        web = pkgs.devshell.mkShell {
+
+        web = pkgs.mkShell {
           packages = with pkgs; [
             nodejs_24
             nodePackages.typescript-language-server
@@ -53,22 +47,15 @@
             shellcheck
             azure-storage-azcopy
           ];
-          env = [
-            {
-              name = "DEVSHELL_NO_MOTD";
-              value = 1;
-            }
-            {
-              name = "PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS";
-              value = 1;
-            }
-            {
-              name = "PATH";
-              eval = "$PATH:node_modules/.bin";
-            }
-          ];
+
+          PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = "1";
+
+          shellHook = ''
+            export PATH="$PATH:node_modules/.bin"
+          '';
         };
-        nvim = pkgs.devshell.mkShell {
+
+        nvim = pkgs.mkShell {
           packages = with pkgs; [
             lua-language-server
             lua54Packages.luacheck
@@ -77,25 +64,36 @@
             gcc
             gnumake
           ];
-          env = [
-            {
-              name = "DEVSHELL_NO_MOTD";
-              value = 1;
-            }
-          ];
         };
-        rust = pkgs.devshell.mkShell {
-          name = "rust";
-          imports = [ "${devshell}/extra/language/rust.nix" ];
-          packages = with pkgs; [ rust-analyzer ];
-          env = [
-            {
-              name = "RUST_BACKTRACE";
-              value = "1";
-            }
+
+        rust = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+            clippy
+            rust-analyzer
+          ];
+
+          RUST_BACKTRACE = "1";
+        };
+
+        linear-term = pkgs.mkShell {
+          packages = [
+            (pkgs.python3.withPackages (
+              p: with p; [
+                textual
+                httpx
+                gql
+                pyyaml
+                platformdirs
+                rich
+              ]
+            ))
           ];
         };
       };
+
       packages.${system} = {
         flattenc = pkgs.writeShellApplication {
           name = "autofollowc";
@@ -104,6 +102,23 @@
         flatteni = pkgs.writeShellApplication {
           name = "autofollowi";
           text = "exec nix run github:fzakaria/nix-auto-follow -- -i flake.lock \"$@\"";
+        };
+        linear-term = pythonPackages.buildPythonApplication {
+          pname = "linear-term";
+          version = "0.1.0";
+          format = "pyproject";
+          src = linear-term;
+          nativeBuildInputs = with pythonPackages; [
+            hatchling
+          ];
+          propagatedBuildInputs = with pythonPackages; [
+            textual
+            httpx
+            gql
+            pyyaml
+            platformdirs
+            rich
+          ];
         };
       };
     };
